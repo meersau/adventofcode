3406aeba334662e1cb6a755933edb6231f9350a8
Alexander Koch <alex@meersau.de>
2018-12-15 23:59:09 +0100

    Day 7 Problem 1 - verannt

--- 07-1/main.go
+++ 07-1/main.go
@@ -7,35 +7,22 @@ import (
 	"os"
 )
 
-type instruction struct {
-	sorted bool
-	steps  *step
-}
-
 type step struct {
-	name   string
-	before *step
-	//next   *step
+	name string
+	next *step
 }
 
 func (s *step) String() string {
-	if s.before == nil {
-		return fmt.Sprintf("Step %s, vor diesem muss nix\n", s.name)
+	if s.next == nil {
+		return fmt.Sprintf("Step %s, kommr keiner mehr", s.name)
 	}
-	return fmt.Sprintf("Step %s, vor diesem muss %s\n", s.name, s.before.name)
+	return fmt.Sprintf("Step %s, hat als nächstes %s", s.name, s.next.name)
 }
 func (s *step) Next() *step {
 	if s == nil {
 		return nil
 	}
-	return s.before
-}
-
-func (s *step) Before() *step {
-	if s == nil {
-		return nil
-	}
-	return s.before
+	return s.next
 }
 
 type steps struct {
@@ -43,35 +30,106 @@ type steps struct {
 }
 
 func (s *steps) Insert(newstep *step) {
-	fmt.Println(newstep)
-	if s.start == nil {
-		s.start = newstep
-		fmt.Printf("Add %s as Start\n", s.start.name)
-		return
-	}
-	if newstep.before.name == s.start.name {
-		fmt.Printf("Neuer Step %s kommt vor %s", newstep.before.name, s.start.name)
-		if s.start.before != nil {
-			fmt.Println("Gibt schon einen :-(")
-			tempbefore := s.start.before
-			s.start.before = newstep
-			newstep.before = tempbefore
-		}
 
-		return
-	}
-	stepbefore := s.Step(newstep.before.name)
-	if stepbefore == nil {
-		fmt.Printf("Kein Step %s gefunden: %v\n", newstep.before.name, newstep)
-		tempstart := s.start
-		s.start = nil
+	//fmt.Println(newstep)
+	if s.start == nil {
+		fmt.Println("START")
 		s.start = newstep
-		fmt.Printf("Neu Start Step %s\n", s.start.name)
-		s.start.before = tempstart
 		return
 	}
-	fmt.Printf("ACHTUNG ÜBERSCHREIBE %v mit %v\n", stepbefore.before, newstep)
-	stepbefore.before = newstep
+
+	if s.start.name == newstep.name {
+		fmt.Println("Muss START ersetzen")
+		if s.start.next == nil {
+			s.start.next = newstep.next
+			return
+		}
+		if s.start.next.name < newstep.next.name {
+			fmt.Println(s.start.next.name, "<", newstep.next.name)
+			if s.start.next.next == nil {
+				fmt.Printf("%s hat keinen nächsten\n", s.start.next.name)
+				s.start.next.next = newstep.next
+				return
+			}
+			newstep.next.next = s.start.next.next
+			s.start.next.next = newstep.next
+
+		} else {
+			newstep.next.next = s.start.next
+			s.start.next = newstep.next
+
+		}
+		return
+	}
+	if newstep.next.name == s.start.name {
+		fmt.Printf("START GLEICH NEW\n")
+		return
+	}
+
+	foundstep := s.Step(newstep.name)
+	if foundstep == nil {
+		fmt.Println("Not yte implemented")
+		return
+	}
+	if foundstep.next == nil {
+		fmt.Println("Gefundener Step hat keinen Nächsten")
+		fmt.Println("Neu: ", newstep)
+		fmt.Println("Gefunden: ", foundstep)
+		gibtesdennächsten := s.Step(newstep.next.name)
+		fmt.Println("Nächster: ", gibtesdennächsten)
+		vorgaenger := s.VorStep(gibtesdennächsten.name)
+		fmt.Println("VORGAENGER", vorgaenger)
+		return
+	}
+
+	if foundstep.next.name == newstep.next.name {
+		return
+	}
+
+	if foundstep.next.name < newstep.next.name {
+		fmt.Println(foundstep.next.name, "<", newstep.next.name)
+		if foundstep.next.next == nil {
+			fmt.Printf("%s hat keinen nächsten\n", foundstep.next.name)
+			foundstep.next.next = newstep.next
+			return
+		}
+		newstep.next.next = foundstep.next.next
+		foundstep.next.next = newstep.next
+		return
+	}
+
+	if foundstep.next.name > newstep.next.name {
+		fmt.Println(foundstep.next.name, ">", newstep.next.name)
+		newstep.next.next = foundstep.next
+		foundstep.next = newstep.next
+		return
+
+	}
+
+	fmt.Println("der keine ahnung was fall")
+	fmt.Println("Neustep: ", newstep)
+	fmt.Println("found: ", foundstep)
+	return
+
+}
+func (ss *steps) VorStep(name string) *step {
+	if ss == nil {
+		return nil
+	}
+	if ss.start.name == name {
+		fmt.Println("START HAT KEINE VORGÄNGER")
+		return nil
+	}
+	cst := ss.start.Next()
+	var vorg *step
+	for cst != nil {
+		if cst.name == name {
+			return vorg
+		}
+		vorg = cst
+		cst = cst.next
+	}
+	return nil
 }
 func (ss *steps) Step(name string) *step {
 	if ss == nil {
@@ -83,11 +141,10 @@ func (ss *steps) Step(name string) *step {
 	cst := ss.start.Next()
 	for cst != nil {
 		//fmt.Println("Found next", cst)
-		// if cst.name == name || cst.before.name == name || cst.next.name == name {
 		if cst.name == name {
 			return cst
 		}
-		cst = cst.before
+		cst = cst.next
 	}
 	return nil
 }
@@ -96,7 +153,7 @@ func (ss *steps) PrintAll() {
 	cst := ss.start
 	for cst != nil {
 		fmt.Printf("%s ", cst.name)
-		fmt.Printf("%v", cst)
+		//fmt.Printf("%v", cst)
 		cst = cst.Next()
 	}
 }
@@ -114,10 +171,14 @@ func main() {
 		fmt.Sscanf(s.Text(), "Step %s must be finished before step %s can begin.", &mustfin, &canbegin)
 		ss.Insert(&step{
 			name: mustfin,
-			before: &step{
+			next: &step{
 				name: canbegin,
 			},
 		})
+		fmt.Println("==========================================")
+		ss.PrintAll()
+		fmt.Println()
+		fmt.Println("==========================================")
 	}
 	ss.PrintAll()
 }
